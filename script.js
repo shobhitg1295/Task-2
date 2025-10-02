af51608c6552885c76f5e1b67a6f6f8f42f70986
shobhitg1295 <shobhitg1295@gmail.com>
2025-10-02 07:13:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    nextArrow: $('.prev')
+    });
+});
